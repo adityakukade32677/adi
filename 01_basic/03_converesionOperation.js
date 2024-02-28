3dd6561d4f25ffebfd12dae60f21f6c57a2c3b14
adityakukade32677 <154678178+adityakukade32677@users.noreply.github.com>
2024-02-28 05:39:02 +0000

    conversion

---
+++ 01_basic/03_converesionOperation.js
@@ -0,0 +1,8 @@
+let score=null
+console.log(typeof score);
+console.log()
+let valueinnumber=Number(score)
+console.log(valueinnumber)
+let isLoggedin=1;
+let Booleanisloggedin=Boolean(isLoggedin)
+console.log(Booleanisloggedin);
