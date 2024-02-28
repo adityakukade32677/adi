0ab09981123a52aad26cd13957e7786137f0018c
adityakukade32677 <154678178+adityakukade32677@users.noreply.github.com>
2024-02-28 05:03:43 +0000

    datatypes and variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,17 @@
+const accountid=9973;
+let accountemail="adityakukade23@gmail.com";
+var accountpassword="12345";
+accountcity="malegaon";
+let accountState;
+
+accountpassword="353"
+console.log(accountpassword)
+// accountid=2;
+accountemail="adirya@gmail.com"
+console.log(accountemail)
+console.log(accountid);
+console.table([accountid,accountemail,accountpassword])
+
+// kabhi bhi var mat use na kare lyuki issue in block scope and functional  scope
+
+console.log(accountState);
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,15 @@
+// "use strict " treat all as newer version of js
+
+// alert(3+3) we are using 
+// node js not browswer
+console.log(3+3);
+console.log("Aditus")
+let name="Adi";
+
+let age =17;
+let isloggedin=false;
+console.log(name)
+console.log(age)
+console.log(isloggedin)
+console.log(typeof age )
+console.log(typeof null )
